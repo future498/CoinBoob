87808e571483f3563b71df0bb5236220c5537fef
future498 <84150453+future498@users.noreply.github.com>
2021-06-30 22:33:02 +0200

    Add files via upload

---
+++ NFT/BoobNFT/IERC165.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.16;
+
+// via https://github.com/arcadeum/multi-token-standard/blob/master/contracts/interfaces/IERC165.sol
+
+/**
+ * @title ERC165
+ * @dev https://github.com/ethereum/EIPs/blob/master/EIPS/eip-165.md
+ */
+interface IERC165 {
+
+    /**
+     * @notice Query if a contract implements an interface
+     * @dev Interface identification is specified in ERC-165. This function
+     * uses less than 30,000 gas
+     * @param _interfaceId The interface identifier, as specified in ERC-165
+     */
+    function supportsInterface(bytes4 _interfaceId)
+    external
+    view
+    returns (bool);
+}
